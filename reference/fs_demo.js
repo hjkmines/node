edae848702489363005b794b148565636227ce27
Tony Kim <hjkmines@gmail.com>
2020-08-05 11:40:17 -0600

    adds os mod

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -6,3 +6,14 @@ fs.mkdir(path.join(__dirname, '/test'), {}, err => {
   if (err) throw err; 
   console.log("Folder created...")
 })
+
+//create and write to file 
+fs.writeFile(path.join(__dirname, '/test', 'hello.txt'), 'Hello', err => 
+if (err) throw err; 
+console.log("file successfully written"))
+
+//Read file 
+fs.readFile(path.join(__dirname, '.test', 'hello.txt'), 'utf8', (err, data) => {
+  if (err) throw err; 
+  console.log(data)
+})
